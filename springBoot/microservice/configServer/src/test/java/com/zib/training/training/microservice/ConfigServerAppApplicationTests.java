1993764b4242740a5fa25b6b8fcd9462dce32bec
İbrahim Halil Koyuncu <ibrahimhkoyuncu@gmail.com>
2021-02-26 23:38:46 +0300

    C-3) Microservices example created about spring cloud config server.

---
+++ springBoot/microservice/configServer/src/test/java/com/zib/training/training/microservice/ConfigServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zib.training.training.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
