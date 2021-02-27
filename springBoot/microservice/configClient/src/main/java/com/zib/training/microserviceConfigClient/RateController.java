1150edd5bb5fc8653a6a80b62444944f9eec734a
İbrahim Halil Koyuncu <ibrahimhkoyuncu@gmail.com>
2021-02-27 13:28:27 +0300

    C-4)Created client microservice.

---
+++ springBoot/microservice/configClient/src/main/java/com/zib/training/microserviceConfigClient/RateController.java
@@ -0,0 +1,34 @@
+package com.zib.training.microserviceConfigClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class RateController {
+
+    @Value("${rate}")
+    String rate;
+
+    @Value("${lanecount}")
+    String lanecount;
+
+    @Value("${tollstart}")
+    String tollstart;
+
+
+    @RequestMapping("/rate")
+    public String getRate(Model m) {
+
+        m.addAttribute("rateamount", rate);
+        m.addAttribute("lanes", lanecount);
+        m.addAttribute("tollstart", tollstart);
+
+        //name of view
+        return "rateView";
+    }
+
+
+}
